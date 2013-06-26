b1ca1dd5326d7cce1037687886189e9843d1ecfc
Kenta <Kenta@192.168.11.17>
2013-06-26 17:41:14 +0900

    test

---
+++ src/test/Kuromoji.java
@@ -0,0 +1,33 @@
+package test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.atilika.kuromoji.Token;
+import org.atilika.kuromoji.Tokenizer;
+
+public class Kuromoji {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Tokenizer tokenizer = Tokenizer.builder().build();
+
+		List<Token> tokens = tokenizer.tokenize("もー眠い");
+
+		for (Token token : tokens) {
+			System.out.println("==================================================");
+			System.out.println("allFeatures : " + token.getAllFeatures());
+			System.out.println("partOfSpeech : " + token.getPartOfSpeech());
+			System.out.println("position : " + token.getPosition());
+			System.out.println("reading : " + token.getReading());
+			System.out.println("surfaceFrom : " + token.getSurfaceForm());
+			System.out.println("allFeaturesArray : " + Arrays.asList(token.getAllFeaturesArray()));
+			System.out.println("辞書にある言葉? : " + token.isKnown());
+			System.out.println("未知語? : " + token.isUnknown());
+			System.out.println("ユーザ定義? : " + token.isUser());
+		}
+	}
+}
